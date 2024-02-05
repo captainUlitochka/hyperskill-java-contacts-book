6dce3de030236087d0f3b158fc9b0ec17d545de6
Olga <snailcaptain@outlook.com>
2024-02-05 20:17:30 +0300

    Stage 3 WIP: remade contact creation, edition, deletion, printing

--- Contacts (Java)/task/src/contacts/Book.java
+++ Contacts (Java)/task/src/contacts/Book.java
@@ -9,6 +9,7 @@ import static java.lang.System.out;
 public class Book {
     List<Contact> contactList;
     private int nextId = 1;
+
     public void start() {
         contactList = new ArrayList<>();
         out.print(Messages.COMMAND_LIST.getMessage());
@@ -25,7 +26,7 @@ public class Book {
                 case "remove" -> removeContact();
                 case "edit" -> editContact();
                 case "count" -> countContacts();
-                case "info" -> showContactInfo();
+                case "info" -> showContactList();
                 case "exit" -> {
                     return;
                 }
@@ -35,7 +36,8 @@ public class Book {
         }
     }
 
-    private void showContactInfo() {
+    private void showContactList() {
+        Scanner contactId = new Scanner(System.in);
         if (!contactList.isEmpty()) {
             for (Contact contact : contactList) {
                 out.printf(
@@ -43,8 +45,19 @@ public class Book {
                         contactList.indexOf(contact) + 1,
                         contact.printContactName());
             }
+            out.println(showContactInfo(contactId.nextInt()));
         } else {
             out.printf(Messages.NOTHING_TO_DO.getMessage(), "show");
+        }
+//TODO: этот метод не должен вызывать следующий, так как используется в редактировании без "прицепа"
+    }
+
+    private String showContactInfo(int contactId) {
+        out.println(Messages.ENTER_CONTACT_ID.getMessage());
+        if (contactId <= contactList.size()) {
+            return contactList.get(contactId + 1).printContactInfo();
+        } else {
+            return Messages.INVALID_CMD.getMessage();
         }
 
     }
@@ -54,58 +67,69 @@ public class Book {
     }
 
     private void editContact() {
-        Scanner numsIn = new Scanner(System.in);
-        if (!contactList.isEmpty()) {
-            Scanner scanner = new Scanner(System.in);
-            showContactInfo();
-            out.print(Messages.SELECT_RECORD.getMessage());
-            int recordId = numsIn.nextInt() - 1;
-
-            if (recordId <= contactList.size() && recordId >= 0) {
-                out.print(Messages.SELECT_FIELD.getMessage());
-                String field = scanner.nextLine();
-                if (field.equals("name")) {
-                    String name = scanner.nextLine();
-                    contactList.get(recordId).setFirstName(name);
-                } else if (field.equals("surname")) {
-                    String surname = scanner.nextLine();
-                    contactList.get(recordId).setLastName(surname);
-                } else if (field.equals("number")) {
-                    String number = scanner.nextLine();
-                    personList.get(recordId).setPhoneNumber(number);
-                } else {
-                    out.printf(Messages.INVALID_DATA.getMessage(), "command");
-                }
-
-            } else out.printf(Messages.INVALID_DATA.getMessage(), "record number");
+        Scanner input = new Scanner(System.in);
+        for (Contact contact : contactList) {
+            contact.printContactName();
+        }
+        out.println(Messages.SELECT_RECORD.getMessage());
+        int contactId = input.nextInt();
+        String fieldName = input.nextLine();
+        String newValue = input.nextLine();
+        if (contactList.get(contactId + 1).editContact(fieldName, newValue)) {
             out.printf(Messages.RECORD_SUCCESS.getMessage(), "updated");
-        } else out.printf(Messages.NOTHING_TO_DO.getMessage(), "edit");
-
+        }
     }
 
     private void removeContact() {
-        Scanner in = new Scanner(System.in);
-        if (!personList.isEmpty()) {
-            showContactInfo();
+        Scanner input = new Scanner(System.in);
+        if (!contactList.isEmpty()) {
+            for (Contact contact : contactList) {
+                contact.printContactName();
+            }
             out.print(Messages.SELECT_RECORD.getMessage());
-            int recordId = in.nextInt() - 1;
+            int contactId = input.nextInt();
 
-            if (recordId <= personList.size() && recordId >= 0) {
-                personList.remove(recordId);
+            if (contactId <= contactList.size() && contactId >= 0) {
+                contactList.remove(contactId - 1);
                 out.printf(Messages.RECORD_SUCCESS.getMessage(), "removed");
             } else out.printf(Messages.INVALID_DATA.getMessage(), "record number");
         } else out.printf(Messages.NOTHING_TO_DO.getMessage(), "remove");
     }
 
     private void createContact() {
-        Scanner in = new Scanner(System.in);
-        out.print(Messages.ENTER_NAME.getMessage());
-        String firstName = in.nextLine();
-        out.print(Messages.ENTER_SURNAME.getMessage());
-        String lastName = in.nextLine();
-        out.print(Messages.ENTER_NUMBER.getMessage());
-        String number = in.nextLine();
+        Scanner input = new Scanner(System.in);
+        out.println(Messages.CHOOSE_CONTACT_TYPE.getMessage());
+        String contactType = input.nextLine();
 
+        if (contactType.equals("person")) {
+            out.printf(Messages.ENTER_DATA.getMessage(), "name");
+            String name = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "surname");
+            String lastName = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "birth date");
+            String birthDate = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "gender (M, F)");
+            String gender = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "number");
+            String personNumber = input.nextLine();
+            Person person = new Person(nextId, name, lastName, birthDate, gender, personNumber);
+            contactList.add(person);
+            nextId++;
+        } else if (contactType.equals("organization")) {
+            out.printf(Messages.ENTER_DATA.getMessage(), "organization name");
+            String orgName = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "address");
+            String address = input.nextLine();
+            out.printf(Messages.ENTER_DATA.getMessage(), "number");
+            String orgNumber = input.nextLine();
+            Organization organization = new Organization(nextId, orgName, address, orgNumber);
+            contactList.add(organization);
+            nextId++;
+        } else {
+            out.printf(Messages.INVALID_DATA.getMessage(), "type");
+        }
+
+        /*
         try {
             Person person = new Person(nextId, firstName, lastName, number);
             nextId++;
@@ -114,9 +138,8 @@ public class Book {
             out.println(e.getMessage());
         }
 
+         */
         out.printf(Messages.RECORD_SUCCESS.getMessage(), "added");
     }
-
-    //TODO: переименовать список
 
 }
